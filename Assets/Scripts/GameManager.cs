7f31f9428c32a793e0978adbfff796338b8f416a
Markus-Kivinen <markus.kivinen2@gmail.com>
2025-11-25 20:45:41 +0200

    add ref to playerData to GameManager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,7 +2,9 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    
+    [Tooltip("Player data scriptable object")]
+    public PlayerData playerData;
+
     public void OnItemPickup(string itemName)
     {
         Debug.Log("GameManager: Item picked up - " + itemName);
